69168586d768605bc53836f35fd6dc326569b9bb
Pablo Insua <pablo.insua.gerpe@gmail.com>
2024-05-29 13:59:05 +0200

    Prueba tecnica con promises y fetchs

---
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -0,0 +1,24 @@
+// Entrar en la api para ver que endpoint nos puede ser util/necesitamos antes de codear
+import './App.css'
+import { useCatImage } from './hooks/useCatImage.js'
+import { useCatFact } from './hooks/useCatFact.js'
+
+export function App () {
+  const { fact, refreshFact } = useCatFact()
+  const { imageUrl } = useCatImage({ fact })
+
+  const handleClick = async () => {
+    refreshFact()
+  }
+
+  return (
+    <main>
+      <h1>App de gatitos</h1>
+
+      <button onClick={handleClick}>Get new fact</button>
+
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three words for ${fact}`} />}
+    </main>
+  )
+}
